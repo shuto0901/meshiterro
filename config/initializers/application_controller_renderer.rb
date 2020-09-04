ddc3deac97012b86ddc568e63583a8dfc0893a12
EC2 Default User <ec2-user@ip-172-31-63-19.ec2.internal>
2020-09-04 15:22:52 +0000

    Initial Commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '303c44de2ae0d73d8d7533431d7c84591e8fef16085a14a479199dd277a242648a3c6d0f4b1fd1498b69f8da86ad099b280a721f7b3c21574b29cd857dd070d5'
